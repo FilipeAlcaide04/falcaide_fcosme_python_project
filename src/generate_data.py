c9674564e942d2d9779395b998f3b5f6b27e61a5
FilipeAlcaide12 <filipealcaide12@gmail.com>
2025-05-22 21:56:52 +0100

    mudei algumas cenas, ver o args parse

--- src/generate_data.py
+++ src/generate_data.py
@@ -1,10 +1,24 @@
 """Aqui serão gerados dados ficticios relativamenta ás eleições legislativas de 2025"""
 
 import json
+import pandas as pd
+import os
+import sys
+import random as r
+import argparse
+
+dd = 'data/raw/partidos.json' #usar argparse
+rr = "data/processed/dados_regionais_processed.csv"
 
 # Tranformei o json com os partidos num dicionário com a Sigla e o Full Name
-with open('data/raw/partidos.json', encoding='utf-8') as f:
+with open(dd, encoding='utf-8') as f:
     parties = json.load(f)
 
-for sigla, nome in parties.items():
-    print(f"{sigla}: {nome}")
+"""for sigla, nome in parties.items():
+    print(f"{sigla}: {nome}")"""
+
+# Pegar nos dados regionais já processados
+
+freguesias = pd.read_csv(rr)
+
+print(freguesias)
