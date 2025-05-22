917d00369856ca50c1beb4742982d5a64955b9f3
FilipeAlcaide12 <filipealcaide12@gmail.com>
2025-05-22 21:02:38 +0100

    adicionei o main script, so para facilitar, may delete later :)

---
+++ main.py
@@ -0,0 +1,8 @@
+""" Script feito para rodar todos os outros script por ordem"""
+import os
+
+os.system("Clear")
+
+os.system("python3 1º-data_aquisition.py")
+os.system("python3 2º-data_cleaning_etc.py")
+os.system("python3 3º-data_disponilization.py") 
